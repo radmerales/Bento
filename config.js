c2e72b904ff27e82faf4b00af5e8465854671bb1
Gerarld Merals <33769613+radmerales@users.noreply.github.com>
2024-09-18 21:07:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Rad',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
@@ -24,16 +24,6 @@ const CONFIG = {
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
-
-	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
-	language: 'en', // More languages in https://openweathermap.org/current#multi
-
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
 
 	// Autochange
 	autoChangeTheme: true,
